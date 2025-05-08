7310328a4180c7ea128ead3bf92b601e731a3ae1
Janez Ugovsek <janez@ugovsek.info>
2025-05-08 10:29:31 +0200

    Added support for build image

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,14 @@ source "config"
 # constants
 DEBIAN_VERSION="bookworm" # bookworm:12
 DEBIAN_ARCH="amd64"
-IMAGE_SIZE=1000 # size ofimage in MB
+IMAGE_SIZE=3000 # size ofimage in MB
 
 # TODO: check GNU/Linux distribution
 
 # TODO: check ROOTFS - must be something except / or empty
+
+# user must be root
+[ "$(id -u)" != "0" ] && exit 1
 
 # check if folders exist
 [ ! -d ${ROOTFS} ] && mkdir ${ROOTFS} &>>/dev/null
@@ -45,6 +48,7 @@ chroot ${ROOTFS} useradd -m -s /bin/bash -p '$y$j9T$tfjQQVBYroUQisXUtyskm.$IZanM
 # copy configurations
 cp -ar files ${ROOTFS}
 chown -R root:root ${ROOTFS}/files
+chown -R 1001:1001 ${ROOTFS}/files/home/janez
 cp -arp ${ROOTFS}/files/* ${ROOTFS}/
 rm -rf ${ROOTFS}/files
 
@@ -60,3 +64,90 @@ chroot ${ROOTFS} apt-get clean
 ######################################
 ########## BUILD IMAGE ###############
 ######################################
+
+# create empty bock file
+dd if=/dev/zero of=${IMAGES}/image.img bs=1M count=${IMAGE_SIZE}
+chmod 666 ${IMAGES}/image.img
+
+# create loopback device
+loopback=$(losetup -f)
+
+if ! losetup ${loopback} ${IMAGES}/image.img
+then
+    # docker workaround
+    lonum=$(echo "${loopback}" | sed 's|/dev/loop||g')
+    mknod -m 660 /dev/loop${lonum} b 7 ${lonum}
+    losetup ${loopback} ${IMAGES}/image.img
+fi
+
+# create GPT partition layout
+echo -e "g
+
+n
+1
+
++64M
+t
+1
+
+n
+2
+
+
+
+x
+A
+1
+r
+
+w" | fdisk ${IMAGES}/image.img
+
+# show all partitions as /dev/loopXp
+partprobe ${loopback}
+partitions=$(lsblk --raw --output "MAJ:MIN" --noheadings ${loopback} | tail -n +2)
+counter=1
+for i in ${partitions}; do
+    maj=$(echo $i | cut -d: -f1)
+    min=$(echo $i | cut -d: -f2)
+    if [ ! -e "${loopback}p${counter}" ]; then echo "mknod ${loopback}p${counter} b ${maj} ${min}" && mknod ${loopback}p${counter} b ${maj} ${min}; fi
+    counter=$((counter + 1))
+done
+
+# create filesystems
+mkfs.fat -F32 -s1 ${loopback}p1
+mkfs.ext4 ${loopback}p2
+
+rootfsuuid=$(blkid -s UUID -o value "${loopback}p2")
+
+mount ${loopback}p1 /mnt
+
+# create legacy boot (syslinux)
+dd bs=440 count=1 conv=notrunc if=/usr/lib/syslinux/mbr/gptmbr.bin of=${loopback}
+mkdir -p /mnt/syslinux /mnt/EFI/boot/
+cp /usr/lib/syslinux/modules/bios/* /mnt/syslinux/
+extlinux --install /mnt/syslinux
+# set boot configuration
+echo -e "DEFAULT boot\nTIMEOUT 0\n\nLABEL boot\n\tLINUX /linux/vmlinuz\n\tINITRD /linux/initrd.img\n\tAPPEND root=UUID=${rootfsuuid} rw\n" > /mnt/syslinux/syslinux.cfg
+
+# create EFI boot (syslinux)
+cp /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi /mnt/EFI/boot/bootx64.efi
+cp /usr/lib/syslinux/modules/efi64/* /mnt/EFI/boot/
+echo -e "DEFAULT boot\nTIMEOUT 0\n\nLABEL boot\n\tLINUX /linux/vmlinuz\n\tINITRD /linux/initrd.img\n\tAPPEND root=UUID=${rootfsuuid} rw\n" > /mnt/EFI/boot/syslinux.cfg
+
+# copy kernel and initramfs to boot partition - TODO: find better solution
+mkdir /mnt/linux
+cp ${ROOTFS}/vmlinuz /mnt/linux
+cp ${ROOTFS}/initrd.img /mnt/linux
+
+umount /mnt
+
+# mount and copy data
+mount ${loopback}p2 /mnt/
+cp -arp ${ROOTFS}/* /mnt/
+umount /mnt
+
+# destroy loopback
+losetup -d ${loopback}
+[ -e "${loopback}" ] && rm ${loopback}
+[ -e "${loopback}p1" ] && rm ${loopback}p1
+[ -e "${loopback}p2" ] && rm ${loopback}p2
